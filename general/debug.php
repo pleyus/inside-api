5ecd241e8c6c595730347bedb66fb45c8d1c9f07
pleyus <pleyus@gmail.com>
2019-01-27 23:23:48 -0600

    Se deja listo el debug
    Para futuros usos...

--- general/debug.php
+++ general/debug.php
@@ -2,16 +2,13 @@
 	//	Terminamos para que no se abra sin login...
 	if ( !defined('MAKE') ) die();
 
-	echo '<pre>';
-	
 	if( USER_LEVEL == UserType::Admin )
 	{
-		print_r(SerializeIdNumber('#_18LCO###', [234,434,455,76,323,784]));
-		// $limit = 2;
 
-		// for($i = 1; $i< 1150;$i++)
-		// 	echo $i . "\t|\t" .  str_pad($i, $limit, "0", STR_PAD_LEFT) . "\t|\t" . substr( str_pad($i, $limit, "0", STR_PAD_LEFT), -$limit) . PHP_EOL;
-		
-		die();	
+		//	Agrega aqui el codigo a testear y luego ve a
+		//	https://unitam.edu.mx/api/?using=general&make=debug
+		//
+		//	Solo los admins...
+
 
 	}
