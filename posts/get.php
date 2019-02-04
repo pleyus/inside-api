84e4a84dc53aa14614c85ab614e875293289d379
pleyus <pleyus@gmail.com>
2019-02-03 22:10:41 -0600

    Creado el make de carga individual

---
+++ posts/get.php
@@ -0,0 +1,49 @@
+<?php
+    //	Terminamos para que no se abra sin login...
+    if ( !defined('MAKE') ) die();
+
+    $id = service_match_param('id');
+
+    if( $id > 0 ) {
+        //  Sacamos la info base del post
+        $post = service_db_select("SELECT * FROM inside_posts WHERE id = :id", ['id' => $id]);
+
+        //  Si es que hay un al menos un post
+        if( !empty($post) ) {
+
+            //  Lo ponemos como post principal
+            $post = $post[0];
+
+            //  Cargamos la imagen como null
+            $post['image'] = null;
+            
+            //  Si tenemos un iid en el post
+            if($post['iid'] > 0)
+            {
+
+                //  Seleccionamos la imagen de acuerdo al iid
+                $q = "SELECT * FROM inside_posts_images WHERE id = :iid";
+                $p = [ 'iid' => $post['iid'] ];
+                $r = service_db_select($q, $p);
+
+                // Y lo asignamos a image
+                $post['image'] = !empty($r) ? $r[0] : null;
+            }
+
+            //  Ahora sacamos todas las imagenes de la galeria (images)
+            $q = "SELECT i.* 
+                FROM 
+                    inside_pi_list l
+                    LEFT JOIN inside_posts_images i ON i.id = l.iid
+                WHERE l.pid = :pid"
+            $p = ['pid' => $post['id']];
+            $r = service_db_select($q, $p);
+
+            //  Y las asignamos
+            $post['images'] = !empty($r) ? $r : [];
+
+            service_end(Status::Success, $post);
+        }
+    }
+    else 
+        service_end(Status::Error, 'No se encuentra el post que has solicitado');
